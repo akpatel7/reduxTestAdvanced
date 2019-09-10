95dd2d4aaca95bc4dab9703241fe8acd51db29e0
Patel, Amitkumar <Amitkumar.Patel@tdassurance.com>
2019-09-10 14:24:32 -0400

    simulate async actions, use thunks

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -31,11 +31,19 @@ export const subtract = (val) => {
     };
 };
 
-export const storeResult = (result) => {
+export const saveResult = ( result ) => {
     return {
         type: STORE_RESULT,
         result
     };
+}
+
+export const storeResult = ( res ) => {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(saveResult( res ))
+        }, 2000);
+    }
 };
 
 
